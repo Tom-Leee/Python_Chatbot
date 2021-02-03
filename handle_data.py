a77deccb75dc0356b9a96e15a92bdf1068884754
Green <joohyoung.t.lee@gmail.com>
2021-02-03 11:47:47 +0900

    2021-02-02

--- handle_data.py
+++ handle_data.py
@@ -37,17 +37,37 @@ try:
     for s in users:
         with db.cursor() as cursor:
             sql = '''
-            
-            '''
+                insert user(name, age, address) values("%s", "%d", "%s")
+            '''% (s['name'], s['age'], s['address'])
 
-    with db.cursor() as cursor:
-
-        # cursor 객체의 execute() 함수로 SQL 구문을 실행합니다
-        cursor.execute(sql)
+            cursor.execute(sql)
 
     # DB호스트에 연결된 객체(db)에 commit()를 통해 수정된 내용을
     # DB에 반영하여 줍니다.
     db.commit()
+
+    # 30대 학생만 조회 ---②
+    cond_age = 30
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = '''
+        select * from user where age > %d
+        ''' % cond_age
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    print(results)  # 가져온 데이터 출력
+
+    cond_name = '에어포트'  # 찾고자 하는 이름
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = '''
+        select * from tb_student where name="%s"
+        ''' % cond_name
+        cursor.execute(sql)
+        result = cursor.fetchone()  # select 구문으로 조회한 데이터 중 하나만 불러오는 함수
+    print(result['name'], result['age'])
+
+    # pandas 데이터 프레임으로 표현
+    df = pd.DataFrame(results)
+    print(df)
 
 except Exception as e:
     # DB 연결 실패 시 오류 내용 출력
@@ -61,5 +81,6 @@ finally:
         # print('table 생성 완료')
         # print('data 삽입 완료')
         # print('data 수정 완료')
-        print('data 삭제 완료')
+        # print('data 삭제 완료')
+        print('data 임의로 DB에 삽입 완료')
         print("DB 연결 닫기 성공")
